23b44192234200e00a0f61d48504be566247172d
atoudic936 <ghostof83@hotmail.fr>
2018-04-06 10:38:53 +0200

    first commit

---
+++ src/main/java/fr/univtln/atoudic936/projetdocker/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.atoudic936.projetdocker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
